1f09efe7af34f949eef90f56dea1344c5fe14197
Hao Huynh <hah.ptit@gmail.com>
2015-09-10 17:43:40 -0700

    Exercise8.7 Input Issue

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 git add --all
 git config --global user.name "Hao Huynh"
 git config --global user.email hah.ptit@gmail.com
-git commit -m "Exercise7.6"
+git commit -m "Exercise8.7 Input Issue"
 git push origin master
